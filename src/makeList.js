8ad3c73402cb86fe51c54a34f23534d0c09db1c0
marshmallow94 <marie.shimizu19@gmail.com>
2023-04-05 13:32:51 -0700

    fixed bug

--- src/makeList.js
+++ src/makeList.js
@@ -2,7 +2,7 @@ import React , {useState} from "react";
 import { Link } from "react-router-dom";
 import "./makeList.css";
 import {API} from "aws-amplify";
-import { create } from "handlebars";
+
 
 /*
 function makeList() {
